52e921e2bad9c8ffc9230ba4f647c7120718dda2
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-09 19:48:03 -0300

    dic dinamicas

--- receitas/views.py
+++ receitas/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    dados = {
+        'nome_da_receitas': {
+            1: 'Lasanha',
+            2: 'Sopa de Legumes',
+            3: 'Sorvete',
+            4: 'Bolo de chocolate'
+        }
+    }
+    return render(request, 'index.html', dados)
 
 
 def receita(request):
     return render(request, 'receita.html')
+
