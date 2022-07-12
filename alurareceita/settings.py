f513f10daf2d53d28f3d366ffd5382c344397e2a
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-11 22:25:56 -0300

    tela cadastro e tela login

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -17,7 +17,6 @@ from alurareceita import static
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -29,10 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
     'pessoas',
     'receitas',
     'django.contrib.admin',
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'alurareceita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')], # avisando pro projeto aonde esta todo o templates
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # avisando pro projeto aonde esta todo o templates
         # html do projeto
         'APP_DIRS': True,
         'OPTIONS': {
@@ -74,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -87,7 +85,6 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -107,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -121,11 +117,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # referenciando os arquivos staticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # referenciando os arquivos staticos
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
