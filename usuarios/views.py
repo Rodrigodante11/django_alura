ee10db493721267cc0e336b5356806ff24096a94
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-29 13:50:51 -0300

    refatorando as views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User  # trazendo os modelos ja cadastrados
+from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
 
@@ -81,61 +81,6 @@ def dashboard(request):
         return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
-
-
-def cria_receita(request):
-    if request.method == 'POST':
-        # Pegando todos os dados do template cria_receita.html
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-
-        user = get_object_or_404(User, pk=request.user.id)  # pegando o usuario logado
-
-        receita = Receita.objects.create(  # criando um objeto de receitas
-            pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo,
-            tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita
-        )
-        receita.save()  # Salvando o objeto no banco de dados
-        return redirect('dashboard')
-
-    else:
-        return render(request, 'receitas/cria_receita.html')
-
-
-def deleta_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita.delete()
-    return redirect('dashboard')
-
-
-def editar_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita_a_editar = {'receita': receita}
-    return render(request, 'receitas/edita_receita.html', receita_a_editar)
-
-
-def atualiza_receita(request):
-    if request.method == 'POST':
-        receita_id = request.POST['receita_id']
-        receita = Receita.objects.get(pk=receita_id)
-
-        receita.nome_receita = request.POST['nome_receita']
-        receita.ingredientes = request.POST['ingredientes']
-        receita.modo_preparo = request.POST['modo_preparo']
-        receita.tempo_preparo = request.POST['tempo_preparo']
-        receita.rendimento = request.POST['rendimento']
-        receita.categoria = request.POST['categoria']
-        if 'foto_receita' in request.FILES:  # se tiver uma foto no campo de foto
-            receita.foto_receita = request.FILES['foto_receita']
-        print(receita.id)
-
-        receita.save()
-        return redirect('dashboard')
 
 
 def campo_vazio(campo):
